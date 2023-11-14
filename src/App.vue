db480aa3d5bf5003331f390cf204b497b97f8722
Stefan-Trajkovski <lgnexus5132@gmail.com>
2023-11-14 21:45:08 +0100

    design update

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,53 @@
 <template>
   <div id="#app">
+    <div class="pTest">
+      Добредојдовте на ClickMaster играта, во оваа игра, во различни интервали,
+      на екранот ќе се појави квадрат, целта е да се кликне на квадратот што е
+      можно побрзо! Брзината на реакција ќе ја одреди гејмерската можност на
+      играчите.
+    </div>
 
-
-    <button v-show="!showPlayAgain" @click="funkcija()" :disabled="buttonDisabled">Стисни да ја играш играта</button>
+    <button
+      v-show="!showPlayAgain"
+      @click="funkcija()"
+      :disabled="buttonDisabled"
+    >
+      Стисни да ја играш играта
+    </button>
     <button v-show="showPlayAgain" @click="funkcija()">Играј повторно</button>
-    <BlockVue :showBlock="showBlock" :vremePristignuvanje="vremePristignuvanje" @bananica="povratBlock"></BlockVue>
+    <BlockVue
+      :showBlock="showBlock"
+      :vremePristignuvanje="vremePristignuvanje"
+      @bananica="povratBlock"
+    ></BlockVue>
 
-    <ResultsVue :razlika="razlika" :prikaziRezultati="prikaziRezultati"></ResultsVue>
-
+    <ResultsVue
+      :razlika="razlika"
+      :prikaziRezultati="prikaziRezultati"
+    ></ResultsVue>
   </div>
 </template>
 
 <script>
-import BlockVue from './components/BlockVue.vue';
-import ResultsVue from './components/ResultsVue.vue';
-
+import BlockVue from "./components/BlockVue.vue";
+import ResultsVue from "./components/ResultsVue.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     BlockVue,
-    ResultsVue
+    ResultsVue,
   },
   methods: {
     funkcija() {
-      this.prikaziRezultati = '';
+      this.prikaziRezultati = "";
       this.buttonDisabled = true;
 
-      const randomNumber = (Math.random() * 2);
+      const randomNumber = Math.random() * 2;
       setTimeout(() => {
         this.showBlock = true;
         this.vremePristignuvanje = Date.now();
-      }, randomNumber * 1000)
-
+      }, randomNumber * 1000);
     },
     povratBlock(value) {
       this.prikaziRezultati = value[0];
@@ -40,31 +55,21 @@ export default {
       this.razlika = (this.vremeStisok - this.vremePristignuvanje) / 1000;
       this.showBlock = false;
       this.showPlayAgain = true;
-
-    }
-
-
-
-
-
+    },
   },
 
   data() {
     return {
       showBlock: false,
-      vremePristignuvanje: '',
-      prikaziRezultati: '',
-      vremeStisok: '',
-      razlika: '',
+      vremePristignuvanje: "",
+      prikaziRezultati: "",
+      vremeStisok: "",
+      razlika: "",
       buttonDisabled: false,
       showPlayAgain: false,
-
-    }
-  }
-
-}
-
-
+    };
+  },
+};
 </script>
 
 <style>
@@ -75,5 +80,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.pTest {
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  color: rgb(208, 208, 152);
+  width: 300px;
+  font-size: 12px;
+  margin: auto;
+  margin-bottom: 20px;
 }
 </style>
